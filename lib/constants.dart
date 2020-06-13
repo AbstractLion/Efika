6ad8ed8ed4aa300291e130840bb925110f5ffbc9
AbstractUltra <45088170+abstractultra@users.noreply.github.com>
2020-06-13 14:07:07 -0400

    changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static const String appName = 'ECORP';
   static const String logoTag = 'near.huscarl.loginsample.logo';
   static const String titleTag = 'near.huscarl.loginsample.title';
+
+  static const Color backgroundColor = Colors.white;
+  static const Color accentColor = Colors.green;
+  static const Color textColor = Colors.black;
 }
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,5 +1,9 @@
+import 'package:efika/models/order.dart';
+import 'package:efika/models/orders.dart';
 import 'package:efika/widgets/efika_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:getflutter/components/card/gf_card.dart';
+import 'package:getflutter/components/list_tile/gf_list_tile.dart';
 
 class OrdersScreen extends StatelessWidget {
   @override
@@ -8,7 +12,17 @@ class OrdersScreen extends StatelessWidget {
       appBar: EfikaAppBar(
         title: Text('Orders'),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: Orders.orders.length,
+        itemBuilder: (BuildContext context, int index) {
+          Order order = Orders.orders[index];
+          return GFCard(
+            title: GFListTile(
+              title: Text(order.id.toString()),
+            ),
+          );
+        },
+      ),
     );
   }
 }
