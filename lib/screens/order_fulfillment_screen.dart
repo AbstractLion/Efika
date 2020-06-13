51eef7450109ab2cf1a09e52a0f457e0a6207173
AbstractUltra <45088170+abstractultra@users.noreply.github.com>
2020-06-13 15:44:10 -0400

    changes

--- lib/screens/order_fulfillment_screen.dart
+++ lib/screens/order_fulfillment_screen.dart
@@ -1,4 +1,11 @@
+import 'package:efika/models/item.dart';
+import 'package:efika/models/order.dart';
+import 'package:efika/models/orders.dart';
 import 'package:flutter/material.dart';
+import 'package:getflutter/components/button/gf_button.dart';
+import 'package:getflutter/components/button/gf_button_bar.dart';
+import 'package:getflutter/components/card/gf_card.dart';
+import 'package:getflutter/components/list_tile/gf_list_tile.dart';
 import '../widgets/efika_app_bar.dart';
 
 class RouteParams {
@@ -11,12 +18,28 @@ class OrderFulfillmentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final RouteParams routeParams = ModalRoute.of(context).settings.arguments;
+    Order order = Orders.orders[routeParams.index];
+    List<Item> items = order.items;
     return Scaffold(
       appBar: EfikaAppBar(
         title: Text('Order Items'),
       ),
-      body: Text(
-        routeParams.index.toString(),
+      body: ListView.builder(
+        itemCount: Orders.orders.length,
+        itemBuilder: (BuildContext context, int index) {
+          Item item = items[index];
+          return GFCard(
+            image: Image.network(item.imageUrl),
+            title: GFListTile(
+              title: Text(item.name),
+            ),
+            content: Column(
+              children: <Widget>[
+                Text("Aisle: ${item.aisle.toString()}"),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
