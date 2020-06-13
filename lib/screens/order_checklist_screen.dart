43a903651191a8e4cc739f0bdd04fe8f7136bb66
AbstractUltra <45088170+abstractultra@users.noreply.github.com>
2020-06-13 14:41:34 -0400

    changes

--- lib/screens/order_checklist_screen.dart
+++ lib/screens/order_checklist_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-
 import '../widgets/efika_app_bar.dart';
+
+class RouteParams {
+  final int index;
+  RouteParams(this.index);
+}
 
 class OrderChecklistScreen extends StatelessWidget {
   static const routeName = '/order_checklist';
-
   @override
   Widget build(BuildContext context) {
+    final RouteParams routeParams = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: EfikaAppBar(
-        title: Text('Orders'),
+        title: Text('Order Items'),
       ),
-      body: Container(),
+      body: Text(
+        routeParams.index.toString(),
+      ),
     );
   }
 }
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,7 +1,10 @@
 import 'package:efika/models/order.dart';
 import 'package:efika/models/orders.dart';
+import 'package:efika/screens/order_checklist_screen.dart';
 import 'package:efika/widgets/efika_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:getflutter/components/button/gf_button.dart';
+import 'package:getflutter/components/button/gf_button_bar.dart';
 import 'package:getflutter/components/card/gf_card.dart';
 import 'package:getflutter/components/list_tile/gf_list_tile.dart';
 
@@ -19,6 +22,23 @@ class OrdersScreen extends StatelessWidget {
           return GFCard(
             title: GFListTile(
               title: Text(order.id.toString()),
+            ),
+            buttonBar: GFButtonBar(
+              alignment: WrapAlignment.center,
+              children: <Widget>[
+                GFButton(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      OrderChecklistScreen.routeName,
+                      arguments: RouteParams(
+                        index,
+                      )
+                    );
+                  },
+                  text: "View Details",
+                )
+              ],
             ),
           );
         },
